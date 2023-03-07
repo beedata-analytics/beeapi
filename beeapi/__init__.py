fe5e944997bc73f458ab93d1f2633c991ebd4884
Gemma Argelés <gargeles@hotmail.com>
2023-03-07 11:31:48 +0100

    Support tertiary_amon_measures endpoint

--- beeapi/__init__.py
+++ beeapi/__init__.py
@@ -148,6 +148,19 @@ class Client(object):
 
         return response
 
+    def put_tertiary(self, data):
+        response = request(
+            "POST",
+            self.base_url + self.endpoints["tertiary"],
+            cookies=self.cookie or self._do_login(),
+            headers=self.http_headers,
+            cert=self.certificate,
+            verify=False,
+            data=dumps(data),
+        )
+
+        return response
+
     def put_community(self, data):
         response = request(
             "POST",
